1676d63127270a7b2a1051cb916e676afa578f24
Gavin Li <craftgavin@gmail.com>
2012-11-12 10:37:36 +0800

    file path changes

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@ $time = microtime();
  */
 chdir(dirname(__DIR__));
 
-define("BASE_PATH", "/Users/gavin/Sites");
+define("BASE_PATH", getenv('BASE_PATH'));
 define("CACHE_PATH", BASE_PATH.'/cms-misc/cache');
 
 include BASE_PATH.'/inc/Zend/Loader/StandardAutoloader.php';
@@ -14,13 +14,13 @@ include BASE_PATH.'/inc/Zend/Loader/StandardAutoloader.php';
 $autoLoader = new Zend\Loader\StandardAutoloader(array(
     'namespaces' => array(
         'Zend'	=> BASE_PATH.'/inc/Zend',
-		'Core'	=> BASE_PATH.'/libraries/common/Core',
+		'Core'	=> BASE_PATH.'/inc/Core',
 		'Fucms' => BASE_PATH.'/library-cms/Fucms',
 		'Brick' => BASE_PATH.'/extension/Brick'
     ),
     'prefixes' => array(
     	'Twig'	=> BASE_PATH.'/inc/Twig',
-    	'App'	=> BASE_PATH.'/libraries/common/App',
+    	'App'	=> BASE_PATH.'/inc/App',
     	'Class' => BASE_PATH.'/library-cms/Class'
     )
 ));
