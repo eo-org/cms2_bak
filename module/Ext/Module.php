853da969360c6834ed7153c9c8c4bbf1905793ff
Gavin Li <craftgavin@gmail.com>
2013-01-30 10:25:06 +0800

    bug fixed

--- module/Ext/Module.php
+++ module/Ext/Module.php
@@ -16,31 +16,27 @@ class Module
 	
 	public function setTwig(MvcEvent $event)
 	{
-		$rm = $event->getRouteMatch();
-		$matchedRouteName = $rm->getMatchedRouteName();
-		if(substr($matchedRouteName, 0, 5) != 'admin' && substr($matchedRouteName, 0, 4) != 'rest') {
-			$application = $event->getApplication();
-			$sm = $application->getServiceManager();
-			$config = $sm->get('Config');
-			
-			$twigEnv = Twig\View::getTwigEnv();
-			$twigEnv->addFilter('outputImage',		new \Twig_Filter_Function('Brick\Helper\Twig\Filter::outputImage'));
-			$twigEnv->addFilter('graphicDataJson',	new \Twig_Filter_Function('Brick\Helper\Twig\Filter::graphicDataJson'));
-			$twigEnv->addFilter('substr',			new \Twig_Filter_Function('Brick\Helper\Twig\Filter::substr'));
-			$twigEnv->addFilter('url',				new \Twig_Filter_Function('Brick\Helper\Twig\Filter::url'));
-			$twigEnv->addFilter('pageLink',			new \Twig_Filter_Function('Brick\Helper\Twig\Filter::pageLink'));
-			$twigEnv->addFilter('translate',		new \Twig_Filter_Function('Brick\Helper\Twig\Filter::translate'));
-			
-			$templateDir = array();
-			$brickPathStackConfig = $config['brick_path_stack'];
-			$fileLoader = new \Twig_Loader_Filesystem($brickPathStackConfig);
-			Twig\View::setFileLoader($fileLoader);
-			
-			$dm = $sm->get('DocumentManager');
-			$loader = new Twig\DatabaseLoader($dm, $fileLoader);
-			
-			$twigEnv->setLoader($loader);
-		}
+		$application = $event->getApplication();
+		$sm = $application->getServiceManager();
+		$config = $sm->get('Config');
+		
+		$twigEnv = Twig\View::getTwigEnv();
+		$twigEnv->addFilter('outputImage',		new \Twig_Filter_Function('Brick\Helper\Twig\Filter::outputImage'));
+		$twigEnv->addFilter('graphicDataJson',	new \Twig_Filter_Function('Brick\Helper\Twig\Filter::graphicDataJson'));
+		$twigEnv->addFilter('substr',			new \Twig_Filter_Function('Brick\Helper\Twig\Filter::substr'));
+		$twigEnv->addFilter('url',				new \Twig_Filter_Function('Brick\Helper\Twig\Filter::url'));
+		$twigEnv->addFilter('pageLink',			new \Twig_Filter_Function('Brick\Helper\Twig\Filter::pageLink'));
+		$twigEnv->addFilter('translate',		new \Twig_Filter_Function('Brick\Helper\Twig\Filter::translate'));
+		
+		$templateDir = array();
+		$brickPathStackConfig = $config['brick_path_stack'];
+		$fileLoader = new \Twig_Loader_Filesystem($brickPathStackConfig);
+		Twig\View::setFileLoader($fileLoader);
+		
+		$dm = $sm->get('DocumentManager');
+		$loader = new Twig\DatabaseLoader($dm, $fileLoader);
+		
+		$twigEnv->setLoader($loader);
 	}
 	
 	public function getConfig()
